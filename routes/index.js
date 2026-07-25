f62a0d3ffc26e8139806fed61730fb4597a93aa5
Anurag Kawade <kawade.anurag901@gmail.com>
2026-07-25 22:00:11 +0530

    brand route added

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,13 @@
 import userRouter from "./user.js";
 import contactRouter from "./contact.js";
 import productRouter from "./product.js";
+import brandRouter from "./Brand.js";
 
 const initRoutes = (app) => {
     app.use("/api/user", userRouter);
     app.use("/api/contact", contactRouter);
     app.use("/api/product", productRouter);
+    app.use("/api/brand", brandRouter);
 }
 console.log("in index route");
 
